0751c60bcc2d110e57c23c1daba2e6c088f2ddcd
Agg3losss <aggeloschatz10@gmail.com>
2025-07-20 16:51:04 +0300

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 <!-- main.js -->
+document.addEventListener('contextmenu', e => e.preventDefault());
 document.addEventListener('keydown', e => {
-  if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && ['I', 'C', 'J'].includes(e.key))) {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'C' || e.key === 'J'))
+  ) {
     e.preventDefault();
   }
 });
-document.addEventListener('contextmenu', e => e.preventDefault());
