b82f6bd05465efcfc8ff58bc7f427ca4bab090d9
Agg3losss <aggeloschatz10@gmail.com>
2025-07-20 16:41:43 +0300

    main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// File: js/main.js
-
-// Block F12, right click, etc.
-document.addEventListener('keydown', function (e) {
+<!-- main.js -->
+document.addEventListener('keydown', e => {
   if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && ['I', 'C', 'J'].includes(e.key))) {
     e.preventDefault();
   }
 });
-
-document.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-});
+document.addEventListener('contextmenu', e => e.preventDefault());
